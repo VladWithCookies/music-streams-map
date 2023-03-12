35b53e015f13766163df0dc8d129bb22e1fe32e9
Vlad Viderko <summervibeswith9pm@gmail.com>
2023-03-12 11:58:42 +0100

    Remove body margin

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  base: '/music-streams-map/',
   plugins: [react()],
   resolve: {
     alias: {
